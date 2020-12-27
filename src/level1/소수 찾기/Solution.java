c2fcb2ebb9897429f72f9589ee1bb937a4215cd0
LEE <odong2@ajou.ac.kr>
2020-12-27 19:12:24 +0900

    소수 찾기

---
+++ src/level1/소수 찾기/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i=2; i<=n; i++) {
+            int cnt = 0;
+            for(int j=1; j<=Math.sqrt(i); j++) {
+                if(i%j==0) cnt++;
+                if(cnt>1) break;
+            }
+            if(cnt==1) answer++;
+        }
+        return answer;
+    }
+}
