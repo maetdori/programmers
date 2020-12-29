9d645760d918c30660e1980cac8d389b514c2ce0
LEE <odong2@ajou.ac.kr>
2020-12-29 12:38:20 +0900

    행렬의 덧셈, x만큼 간격이 있는 n개의 숫자. 예산

---
+++ src/level1/예산/Solution.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        int left = budget;
+        Arrays.sort(d);
+        
+        for(int i=0; i<d.length; i++) {
+            if(d[i]<=left)
+                left -= d[i];
+            else break;
+            answer = i+1;
+        }
+        
+        return answer;
+    }
+}
---
+++ src/level1/행렬의 덧셈/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int rowSize = arr1.length;
+        int colSize = arr1[0].length;
+        int[][] answer = new int[rowSize][colSize];
+        
+        for(int i=0; i<rowSize; i++) {
+            for(int j=0; j<colSize; j++) {
+                answer[i][j] = arr1[i][j] + arr2[i][j];
+            }    
+        }
+        
+        return answer;
+    }
+}
