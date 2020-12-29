67b561d93f1c17e79b09c03e1b5daa1cb4fdcf41
LEE <odong2@ajou.ac.kr>
2020-12-29 12:21:10 +0900

    키패드 누르기

--- src/level1/키패드 누르기/Solution.java
+++ src/level1/키패드 누르기/Solution.java
@@ -1,42 +1,61 @@
 class Solution {
-    static int[] bucket = new int[1000];
-    static int cnt;
-    public int solution(int[][] board, int[] moves) {
-        int answer = 0;
-        cnt = 0;
+    public String solution(int[] numbers, String hand) {
+        String answer = "";
         
-        for(int e: moves) {
-            for(int i=0; i<board.length; i++) {
-                if(board[i][e-1] != 0) {
-                    push(board[i][e-1]);
-                    board[i][e-1] = 0;
+        int leftX = 0; //왼손 위치. *의 위치를 (0,0)라 하자.
+        int leftY = 0;
+      
+        int rightX = 2; //오른손 위치.
+        int rightY = 0;
+        
+        int[] xPos = new int[] {1,0,1,2,0,1,2,0,1,2}; //0~9 키패드의 x좌표
+        int[] yPos = new int[] {0,3,3,3,2,2,2,1,1,1}; //0~9 키패드의 y좌표
+        
+        for(int i=0; i<numbers.length; i++) {
+            int next = numbers[i];
+            
+            switch(xPos[next]) { 
+            
+                case 0:
+                    answer += "L";
+                    leftX = xPos[next];
+                    leftY = yPos[next];
                     break;
-                }
+                case 2:
+                    answer += "R";
+                    rightX = xPos[next];
+                    rightY = yPos[next];
+                    break;
+                default:
+                    int lDiffer = 
+                        Math.abs(xPos[next]-leftX) + Math.abs(yPos[next]-leftY);
+                    int rDiffer = 
+                        Math.abs(xPos[next]-rightX) + Math.abs(yPos[next]-rightY);
+                    if(lDiffer == rDiffer) {
+                        if(hand.equals("left")) {
+                            answer += "L";
+                            leftX = xPos[next];
+                            leftY = yPos[next];
+                        }
+                        else {
+                            answer += "R";
+                            rightX = xPos[next];
+                            rightY = yPos[next];
+                        }
+                    }
+                    else if (lDiffer<rDiffer) {
+                        answer += "L";
+                        leftX = xPos[next];
+                        leftY = yPos[next];
+                    }
+                    else {
+                        answer += "R";
+                        rightX = xPos[next];
+                        rightY = yPos[next];
+                    }
+                    break;
             }
-            
-            if(cnt<2) 
-                continue;
-            else if(bucket[cnt-2] == bucket[cnt-1]) {
-                pop();
-                answer += 2;
-            }
-            
-            for(int i=0; i<15; i++) {
-                System.out.print(bucket[i] + " ");
-            }
-            System.out.println();
         }
-        
         return answer;
-    }
-    
-    public void push(int e) {
-        bucket[cnt] = e;
-        cnt++;
-    }
-    
-    public void pop() {
-        bucket[--cnt] = 0;
-        bucket[--cnt] = 0;
     }
 }
