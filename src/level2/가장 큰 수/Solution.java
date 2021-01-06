a23ddd1e6157b4627c8f68665f5574c10ee962bd
LEE <odong2@ajou.ac.kr>
2021-01-07 01:05:36 +0900

    가장 큰 수

--- src/level2/가장 큰 수/Solution.java
+++ src/level2/가장 큰 수/Solution.java
@@ -1,42 +1,27 @@
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 
 class Solution {
-    public int[] solution(int[] progresses, int[] speeds) {
-        List<Integer> speed = new ArrayList<>(); //speeds를 저장할 list
-        List<Integer> status = new ArrayList<>(); //진행 상태 update
-        List<Integer> deploy = new ArrayList<>(); //배포 기능 수
-        int finished = 0; //개발 완료된 기능 수
+    public String solution(int[] numbers) {
+        String answer = "";
+        String[] str = new String[numbers.length];
         
-        for(int i=0; i<speeds.length; i++) {
-            status.add(progresses[i]);
-            speed.add(speeds[i]);
+        for(int i=0; i<numbers.length; i++) {
+            str[i] = Integer.toString(numbers[i]);
         }
         
-        while(!status.isEmpty()) {
-            
-            if(status.get(0) >= 100) { //0번 째 기능이 100% 이상일 때
-                status.remove(0);
-                speed.remove(0);
-                finished++;
-                continue; 
+        Arrays.sort(str, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                return (s2+s1).compareTo(s1+s2);
             }
-            
-            if(finished > 0) { //배포할 수 있는 기능이 1개 이상일 때 
-                deploy.add(finished);
-                finished = 0;
-            }
-            
-            for(int i=0; i<status.size(); i++) { 
-                status.set(i, status.get(i)+speed.get(i)); //진행 상태 update
-            }
-        }
-        deploy.add(finished); //배포할 수 있는 기능 수 저장
+        });
         
-        int[] answer = new int[deploy.size()];
-        for(int i=0; i<deploy.size(); i++) {
-            answer[i] = deploy.get(i);
-        }    
+        if(str[0].equals("0")) return str[0]; //str의 원소가 모두 0이면 "0"을 리턴
+        
+        for(int i=0; i<str.length; i++) {
+            answer += str[i];
+        }
+        
         return answer;
     }
 }
