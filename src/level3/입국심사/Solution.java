fc540d2b6fc6671a8da32b5e8839e4a90a11592e
LEE <odong2@ajou.ac.kr>
2021-05-04 16:52:46 +0900

    입국심사

---
+++ src/level3/입국심사/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public long solution(int n, int[] times) {
+        long answer = Long.MAX_VALUE;
+        long start = times[0];
+        long end = (long)times[times.length-1] * n;
+        
+        while(start <= end) {
+            long done = 0;
+            long mid = (start + end) / 2;
+            
+            for(int time: times) {
+                done += mid / time;
+            }
+            
+            if(done >= n) {
+                answer = mid;
+                end = mid-1;
+            }
+            else start = mid+1;
+        }
+        
+        return answer;
+    }
+}
